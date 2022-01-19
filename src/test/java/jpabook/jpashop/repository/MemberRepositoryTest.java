e928fe2a6a4fcf962d0b307645ce0f86d80c881d
zmfl1230 <zmfl123097@gmail.com>
2022-01-19 16:25:14 +0900

    feat: Member 저장 및 조회 관련 테스트 코드 작성

---
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired
+    ApplicationContext ac;
+
+    @Test
+    public void getAllBeans() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanName: beanDefinitionNames) {
+            System.out.println("beanName = " + beanName);
+        }
+
+    }
+    @Test
+    @Transactional
+    public void saveMember() {
+        //given
+        Member member = new Member();
+        member.setName("memberA");
+
+        //when
+        long memberId = memberRepository.save(member);
+        Member findMember = memberRepository.find(memberId);
+
+        //then
+        Assertions.assertThat(memberId).isEqualTo(findMember.getId());
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
+
+}
