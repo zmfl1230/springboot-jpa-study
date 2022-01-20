93669c5e76daaec5bbbec5e034422cedf6ba0652
zmfl1230 <zmfl123097@gmail.com>
2022-01-21 02:19:00 +0900

    feat: [간단한 회원-주문 서비스 구현을 위한 도메인 생성] OrderItem 도메인 생성

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;
+
+    private int quantity;
+}
