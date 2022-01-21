c847ecfc1e4a1b560bd0ea798022303eef46805b
zmfl1230 <zmfl123097@gmail.com>
2022-01-21 14:48:45 +0900

    feat: [간단한 회원-주문 서비스 구현을 위한 도메인 생성] @ManyToOne, @OneToMany에 대한 생각 정리

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -15,6 +15,12 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    /**
+     * Order를 필드로 가지고 있다는 의미
+     * Order 입자에서는 자신을 가지고 있는 OrderItem이 여러개 있을 가능성이 있다.
+     * 즉, 하나의 Order에 여러개의 OrderItem이 연결되는 댜대일 관계가 형성. 고로, OrderItem 입장에선 @ManyToOne 이 맞음
+     * 반대로 Order에 입장에서 본인은 하나인데 여러개의 OrderItem이 본인을 가리키는 입장이므로 @OneToMany가 맞음
+     * */
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
