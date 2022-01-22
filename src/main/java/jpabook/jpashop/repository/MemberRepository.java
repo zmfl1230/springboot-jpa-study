0c1eeaf097551200b82bd45d2a8a7fc9f8491688
zmfl1230 <zmfl123097@gmail.com>
2022-01-22 17:45:58 +0900

    feat: [MemberRepository] 이미 존재하는 이름인지 조사하는 다양한 메서드 추가   - findByName   - getCount   - isExistByName

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.entity.Member;
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -19,5 +20,28 @@ public class MemberRepository {
 
     public Member find(long memberId) {
         return em.find(Member.class, memberId);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name=:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+    public int getCount(String name) {
+        return em.createQuery("select count(m) from Member m where m.name=:name", Integer.class)
+                .setParameter("name", name)
+                .getSingleResult();
+    }
+
+    public Boolean isExistByName(String name) {
+        return em.createQuery("select count(m) from Member m where m.name=:name", Long.class)
+                .setParameter("name", name)
+                .getSingleResult() != 0;
     }
 }
